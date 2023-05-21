fa39f5bcd2e0f1eee28b3d433d7f4d6ba20a7e93
Kyna2k <huynobi1809@gmail.com>
2023-05-22 04:03:55 +0700

    chan

---
+++ app/src/main/java/com/example/hstalk_version2/SelectLoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.hstalk_version2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_login);
+    }
+}
