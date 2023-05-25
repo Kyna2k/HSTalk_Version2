2f0eb7fcb7b62223f7dd47461fbbc9be82e3eb80
Kyna2k <huynobi1809@gmail.com>
2023-05-25 17:04:12 +0700

    sap xong logn

--- app/src/main/java/com/example/hstalk_version2/views/LoginWithMailActivity.java
+++ app/src/main/java/com/example/hstalk_version2/views/LoginWithMailActivity.java
@@ -3,14 +3,44 @@ package com.example.hstalk_version2.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.hstalk_version2.R;
+import com.example.hstalk_version2.databinding.ActivityLoginWithMailBinding;
 
 public class LoginWithMailActivity extends AppCompatActivity {
-
+    ActivityLoginWithMailBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_with_mail);
+        binding = ActivityLoginWithMailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.username.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(b)
+                {
+                    Log.i("CHECKS", "onFocusChange: ");
+                    binding.edtUsername.setStartIconDrawable(null);
+                }else {
+                    if(!binding.username.getText().toString().equals("")) return;
+                    binding.edtUsername.setStartIconDrawable(R.drawable.rezide_icon_human);
+                }
+            }
+        });
+        binding.password.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(b)
+                {
+                    Log.i("CHECKS", "onFocusChange: ");
+                    binding.edtPass.setStartIconDrawable(null);
+                }else {
+                    if(!binding.password.getText().toString().equals("")) return;
+                    binding.edtPass.setStartIconDrawable(R.drawable.rezide_icon_lock);
+                }
+            }
+        });
     }
 }
