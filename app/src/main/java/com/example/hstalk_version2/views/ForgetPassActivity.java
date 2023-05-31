6445ce6d053e6e7376981a2bd3001a45d122139d
Kyna2k <huynobi1809@gmail.com>
2023-05-31 18:14:35 +0700

    done forget

--- app/src/main/java/com/example/hstalk_version2/views/ForgetPassActivity.java
+++ app/src/main/java/com/example/hstalk_version2/views/ForgetPassActivity.java
@@ -3,14 +3,37 @@ package com.example.hstalk_version2.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.example.hstalk_version2.R;
+import com.example.hstalk_version2.databinding.ActivityForgetPassBinding;
 
 public class ForgetPassActivity extends AppCompatActivity {
-
+    ActivityForgetPassBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forget_pass);
+        binding = ActivityForgetPassBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.username.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(b)
+                {
+                    Log.i("CHECKS", "onFocusChange: ");
+                    binding.edtUsername.setStartIconDrawable(null);
+                }else {
+                    if(!binding.username.getText().toString().equals("")) return;
+                    binding.edtUsername.setStartIconDrawable(R.drawable.rezide_icon_phone2);
+                }
+            }
+        });
+        binding.btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
